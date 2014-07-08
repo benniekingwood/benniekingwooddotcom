9064583208a10c0ec6800bbe9ffa7e766382fa55
Bennie Kingwood <bennie.kingwood@gmail.com>
2014-07-07 22:05:21 -0700

    Updates for mobile UI.

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,27 @@
 $("section").click(function(event) {
+  var contentFadeIn = ($.browser.desktop) ? 500 : 250;
+  var contentFadeOut = ($.browser.desktop) ? 100 : 50;
+  var sectionFadeIn = ($.browser.desktop) ? 1900 : 500;
+  var sectionFadeInTimeout = ($.browser.desktop) ? 2000 : 550;
+  
   var clickedSectionId = this.id;
   if($(this).hasClass("col-md-1")) {
 
     $(this).addClass("col-md-12");
     $(this).removeClass("col-md-1");
 
-    $('section').not(this).each(function(){ $(this).fadeOut(100);});
+    $('section').not(this).each(function(){ $(this).fadeOut(contentFadeOut);});
 
     setTimeout(function() {
-      $("#"+clickedSectionId + " .content-header").fadeIn(500);
-      $("#"+clickedSectionId + " .section-details").fadeIn(500);
-    }, 2000);
+      $("#"+clickedSectionId + " .content-header").fadeIn(contentFadeIn);
+      $("#"+clickedSectionId + " .section-details").fadeIn(contentFadeIn);
+    }, sectionFadeInTimeout);
   } else {
-    $("#"+clickedSectionId + " .content-header").fadeOut(100);
-    $("#"+clickedSectionId + " .section-details").fadeOut(100);
-    $('section').not(this).each(function(){ $(this).fadeIn(1900);});
+    $("#"+clickedSectionId + " .content-header").fadeOut(contentFadeOut);
+    $("#"+clickedSectionId + " .section-details").fadeOut(contentFadeOut);
+    $('section').not(this).each(function(){ $(this).fadeIn(sectionFadeIn);});
     $(this).removeClass("col-md-12");
     $(this).addClass("col-md-1");
+    if($.browser.mobile) $(document).scrollTop($("#"+clickedSectionId ).position().top);
   }
 });
